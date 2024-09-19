dbe520c19d3a44b43f0fb967863fc5d106add8e3
SolveigJorgensen <113687142+SolveigJorgensen@users.noreply.github.com>
2024-09-19 11:49:14 +0200

    Lagt til endring av språk

--- app/src/main/java/com/example/mappe1_s305161/preferanser.java
+++ app/src/main/java/com/example/mappe1_s305161/preferanser.java
@@ -1,12 +1,22 @@
 package com.example.mappe1_s305161;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.graphics.Insets;
+import androidx.core.os.LocaleListCompat;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Locale;
 
 public class preferanser extends AppCompatActivity {
 
@@ -20,5 +30,25 @@ public class preferanser extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ImageButton norskBtn = findViewById(R.id.norskBtn);
+        ImageButton tyskBtn = findViewById(R.id.tyskBtn);
+
+        norskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocaleListCompat appLocale = LocaleListCompat.forLanguageTags("no");
+                AppCompatDelegate.setApplicationLocales(appLocale);
+            }
+        });
+
+        tyskBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                LocaleListCompat appLocale = LocaleListCompat.forLanguageTags("de");
+                AppCompatDelegate.setApplicationLocales(appLocale);
+            }
+        });
+
     }
 }
